e9eeb9ea8001fef9e474179cac44da449fafbc4e
Vincent Villaluna <villalunavincent@gmail.com>
2020-07-15 00:02:50 +0800

    :boom: update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import Header from "./components/Header";
+import PortfolioList from "./components/PortfolioList";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Fragment>
+      <div className="container">
+        <Header />
+        <PortfolioList />
+      </div>
+    </Fragment>
+  )
+};
 
 export default App;
