718c112e7656ed2c2f02fd5f00a31511e96e957d
Vincent Villaluna <villalunavincent@gmail.com>
2020-07-15 00:02:32 +0800

    :heavy_plus_sign: added components

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../assets/css/navigation.css";
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navigation;
