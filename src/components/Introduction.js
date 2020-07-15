05a8f3d8681dea65e7cd943d3c389fe97579db0b
Vincent Villaluna <villalunavincent@gmail.com>
2020-07-15 22:29:18 +0800

    :heavy_plus_sign: added more components

---
+++ src/components/Introduction.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../assets/css/introduction.css";
+
+const Introduction = () => {
+  return (
+    <div className="introduction">
+      <h1>MY INTRODUCTION</h1>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    </div>
+
+  )
+}
+
+export default Introduction;
